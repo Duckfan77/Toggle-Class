aa60f6c63e2625722ff63ac010ef3bc794f46f61
Duckfan77 <cwenthin@yahoo.com>
2016-01-10 14:28:13 -0600

    Creation of necessary files
    Created the ToggleClass.h and .cpp

---
+++ Toggle-Class/src/ToggleClass.cpp
@@ -0,0 +1,10 @@
+/*
+ * ToggleClass.cpp
+ *
+ *  Created on: Jan 10, 2016
+ *      Author: Colin
+ */
+
+#include "ToggleClass.h"
+
+
---
+++ Toggle-Class/src/ToggleClass.h
@@ -0,0 +1,22 @@
+/*
+ * ToggleClass.h
+ *
+ *  Created on: Jan 10, 2016
+ *      Author: Colin
+ */
+
+#ifndef TOGGLECLASS_H_
+#define TOGGLECLASS_H_
+
+class Toggle
+{
+private:
+	Toggle(){
+
+	}
+
+public:
+
+
+};
+#endif /* SRC_TOGGLECLASS_H_ */
