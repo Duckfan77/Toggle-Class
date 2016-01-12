31c50da42af9734536cb90b5c721128da72660ff
Duckfan77 <cwenthin@yahoo.com>
2016-01-12 16:19:24 -0600

    Implemented Falling and dual edge detection
    Implemented functions for detecting falling edges and dual edges.
    Updated .gitignore.

--- Toggle-Class/src/ToggleClass.h
+++ Toggle-Class/src/ToggleClass.h
@@ -16,12 +16,15 @@ private:
 	int m_toggleVal2;
 
 	bool m_risingEdgeMem;
+	bool m_fallingEdgeMem;
 public:
 	//toggle1Val is the current value passed to the function being toggled between two values,
 	//while toggle2Val will be the value returned after the first time the toggle runs, switching between
 	//them on successive toggles.
 	Toggle(int toggle1Val, int toggle2Val);
 	bool risingEdge(bool varToEdgeCheck);
+	bool fallingEdge(bool varToEdgeCheck);
+	bool dualEdge (bool varToEdgeCheck);
 	void toggleStatus();
 	int toggleStatusOnEdgeChange(bool varToEdgeCheck);
 
