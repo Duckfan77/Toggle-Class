648aa2f4fa5f85cd36a4f32f8d5a4a7abccbc97c
Duckfan77 <cwenthin@yahoo.com>
2016-01-12 16:53:59 -0600

    Transformed Toggle to a template
    Changed the return type on the toggleOnEdgeChange function and getState
    to T and the input type on the toggled values to T in the constructor.

--- Toggle-Class/src/ToggleClass.h
+++ Toggle-Class/src/ToggleClass.h
@@ -8,12 +8,13 @@
 #ifndef TOGGLECLASS_H
 #define TOGGLECLASS_H
 
+template <typename T>
 class Toggle
 {
 private:
-	int m_status;
-	int m_toggleVal1;
-	int m_toggleVal2;
+	T m_status;
+	T m_toggleVal1;
+	T m_toggleVal2;
 
 	bool m_risingEdgeMem;
 	bool m_fallingEdgeMem;
@@ -22,7 +23,7 @@ public:
 	//toggle1Val is the current value passed to the function being toggled between two values,
 	//while toggle2Val will be the value returned after the first time the toggle runs, switching between
 	//them on successive toggles.
-	Toggle(int toggle1Val, int toggle2Val);
+	Toggle(T toggle1Val, T toggle2Val);
 	bool risingEdge(bool varToEdgeCheck);
 	bool fallingEdge(bool varToEdgeCheck);
 	bool dualEdge (bool varToEdgeCheck);
@@ -32,9 +33,9 @@ public:
 	//-1 being falling edge, and 0 being dual edge.
 	//The default value is 1, or rising edge.
 	//If a nonvalid value is passed to edgeMode, no edge checking or value toggling takes place;
-	int toggleStatusOnEdgeChange(bool varToEdgeCheck, int edgeMode = 1);
+	T toggleStatusOnEdgeChange(bool varToEdgeCheck, int edgeMode = 1);
 
-	int getStatus(){
+	T getStatus(){
 		return m_status;
 	}
 };
